f7d8fda6442d64ff25fb5996d40ac02ac3598a2c
radhakrishnananusiya-droid <radhakrishnananusiya@gmail.com>
2025-11-06 23:13:31 -0800

    Add files via upload

---
+++ Food if condition.py
@@ -0,0 +1,10 @@
+food = input("What's the best food?:")
+
+if food == "rice":
+    print("Yes, rice is the best.")
+
+elif food == "apple":
+    print("Apples aren't my cup of rice.")
+
+else:
+    print("Never heard of it!")
