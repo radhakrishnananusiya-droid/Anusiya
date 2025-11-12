cda9b1183bff09297fd37c28cc8ee11b356d49b2
radhakrishnananusiya-droid <radhakrishnananusiya@gmail.com>
2025-11-12 14:58:12 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,10 @@
+import requests
+url = input("Enter website URL;")
+try:
+   response = requests.get(url)
+   if response.status_code == 200:
+     print("Website is UP")
+   else:
+     print("Website returned error:", response.status_code)
+except:
+     print("Website is DOWN or unreachable")
